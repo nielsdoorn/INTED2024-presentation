49fbc71ee9846e914c30e7e2adec0c2b8950f7a4
Niels Doorn <nielsdoorn@users.noreply.github.com>
2024-03-04 12:16:00 +0100

    done

--- presentation/build.sh
+++ presentation/build.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-echo lualatex run 1
-lualatex presentation.tex >/dev/null
+echo lualatex run 1/3
+lualatex presentation.tex
 echo biber run
 biber presentation >/dev/null
 echo lualatex run 2
 lualatex presentation.tex >/dev/null
-echo lualatex run 3
+echo final lualatex run
 lualatex presentation.tex >/dev/null
