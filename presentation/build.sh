49a93c77ae0901563cef0b867bd29084c00a3feb
Niels Doorn <nielsdoorn@users.noreply.github.com>
2024-03-04 11:25:41 +0100

    final touches

--- presentation/build.sh
+++ presentation/build.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
-lualatex presentation.tex
-biber presentation
-lualatex presentation.tex
-lualatex presentation.tex
+echo lualatex run 1
+lualatex presentation.tex >/dev/null
+echo biber run
+biber presentation >/dev/null
+echo lualatex run 2
+lualatex presentation.tex >/dev/null
+echo lualatex run 3
+lualatex presentation.tex >/dev/null
